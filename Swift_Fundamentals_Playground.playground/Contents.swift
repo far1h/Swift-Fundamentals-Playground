e5be91aef60f34ffeb1c5b890aa1b59dcdec5b98
far1h <farihmuhammad45@gmail.com>
2024-08-18 23:33:17 +0700

    Constants, variables, operators

--- Swift_Fundamentals_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Playground.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+print(greeting)
+
+//let name = "Jason"
+var name = "Jason"
+
+name = "Amy"
+
+var weight = 100
+var additionalWeight = 50
+
+//weight = weight + additionalWeight
+weight += additionalWeight
+weight -= additionalWeight
+weight /= additionalWeight
+weight *= additionalWeight
+weight %= additionalWeight
+
+var lastName: String = "Smith"
+//lastName = 10 // ERROR
+
+var age: Int = 10
+//age = 5.3 // ERROR
+age = Int(5.4) // ERROR
