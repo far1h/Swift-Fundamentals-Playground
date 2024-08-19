d6790672ccea282aaec34bdf00c7f2008f7bf45d
far1h <farihmuhammad45@gmail.com>
2024-08-19 09:11:22 +0700

    Strings and characters

--- Swift_Fundamentals_Playground.playground/Contents.swift
+++ Swift_Fundamentals_Playground.playground/Contents.swift
@@ -25,3 +25,88 @@ var lastName: String = "Smith"
 var age: Int = 10
 //age = 5.3 // ERROR
 age = Int(5.4) // ERROR
+
+//---------//
+// Strings //
+//---------//
+
+// multiline string
+print("Multi line string")
+var address = """
+123 Main St
+Apt. 483
+Mainville, CA
+"""
+print(address)
+print("-------------")
+
+print("Unicode based characters")
+/// special character string
+let specialChars = "\u{29}\u{2680}"
+print(specialChars)
+print("-------------")
+
+print("Multi line vs extended delimiters")
+/// multie line vs extended delimiters
+address = "124 Oak St.\nOakville, CA"
+print(address)
+address = #"124 Oak St.\nOakville, CA"#
+print(address)
+print("-------------")
+
+/// Empty checks
+let emptyName = String()
+if emptyName.isEmpty {
+  print("emptyName is empty")
+}
+print("-------------")
+
+print("String concatenation")
+
+/// String concatenation
+var sentence = "We are learning "
+sentence += "Swift programming language"
+print(sentence)
+print("-------------")
+
+/// Iteration over characters in a string
+print("iterate over sentence using for in loop")
+for char in sentence {
+  print(char)
+}
+print("-------------")
+
+/// Single character
+let singleCharacter: Character = "A"
+
+/// Collection / array of characters
+let arrayOfCharacters: [Character] = ["A", "n", "i", "m", "a", "l"]
+var charactersStr = String(arrayOfCharacters)
+
+/// Concatenate character to a string
+let questionMark: Character = "?"
+charactersStr.append(questionMark)
+
+print("charactersStr = \(charactersStr)")
+
+/// Interpolation of string
+let animalCount = "no"
+print("There are \(animalCount) animals in the park")
+print("-------------")
+
+/// Swift String & Characters are fully Unicode compliant
+/// Extended grapheme cluster is a sequence of one or more Unicode scalars, e.g. \u{E9}
+/// Character: a single extended grapheme cluster
+let eAcute: Character = "\u{E9}"                         // é
+let combinedEAcute: Character = "\u{65}\u{301}"          // e followed by
+let precomposed: Character = "\u{D55C}"                  // 한
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"   // ᄒ, ᅡ, ᆫ
+
+/// String Indices
+var continent = "North America"
+let startIndex = continent.startIndex
+
+print("start index = \(startIndex)")
+print(continent[startIndex])
+print("-------------")
+
